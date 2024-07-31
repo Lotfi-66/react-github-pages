1f6f2e890737b28a8e191146243e977ebe18960f
Djeghbala Lotfi <lotfi.djeg@gmail.com>
2024-07-31 22:05:17 +0200

    First deploy App

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.css'; // Assurez-vous que cette ligne est présente
 import App from './App';
 
 ReactDOM.render(
